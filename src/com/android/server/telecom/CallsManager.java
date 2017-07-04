05eb743ab7825d505b245dff466c64b2bd12842d
Mengjun Leng <mengju@codeaurora.org>
2017-07-04 11:20:05 +0800

    Telecom: Reset the duration after the CDMA call is accepted
    In android original design, the duration of CDMA MO call is started from
    the dial command sent, so it is not the real duration of the active time.
    
    In this patch, CallsManager receive a new message to
    reset the connection time for this call.
    
    Change-Id: Ib90283a3b5bbc7e8fa1d1e7ef09342ac19302f9c
    CRs-Fixed: 754497

--- src/com/android/server/telecom/CallsManager.java
+++ src/com/android/server/telecom/CallsManager.java
@@ -3075,4 +3075,13 @@ public class CallsManager extends Call.ListenerBase
         }
         return ;
     }
+
+    void resetCdmaConnectionTime(Call call) {
+        call.setConnectTimeMillis(System.currentTimeMillis());
+        if (mCalls.contains(call)) {
+            for (CallsManagerListener listener : mListeners) {
+                listener.onCallStateChanged(call, CallState.ACTIVE, CallState.ACTIVE);
+            }
+        }
+    }
 }
